7aad3b96f91fa296fa0e48f56bee67c7d9e2feab
Yumax-panda <yuma0wall@gmail.com>
2023-06-21 15:06:45 +0900

    impl

---
+++ ch03/step_function.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def step_function(x: np.ndarray) -> np.ndarray:
+    return (x>0).astype(np.int64)
+
+def sigmoid(x :np.ndarray) -> np.ndarray:
+    return 1/(1+np.exp(-x))
+
+
+if __name__ == '__main__':
+    x = np.arange(-5.0, 5.0, 0.1)
+    y = step_function(x)
+    plt.plot(x, y)
+    plt.ylim(-0.1, 1.1)
+    plt.show()
