985c80e178bb8d833ae8b43b94b009f191ed613b
Yumax-panda <yuma0wall@gmail.com>
2023-06-22 18:17:40 +0900

    implemented gradient descent

--- ch04/gradient.py
+++ ch04/gradient.py
@@ -16,6 +16,33 @@ def gradient(f: F, x: np.ndarray) -> np.ndarray:
     return grad
 
 
+def gradient_descent(
+    f: F,
+    x: np.ndarray,
+    rate: float = 0.01,
+    step: int = 100
+) -> tuple[np.ndarray, list[list[float, float]]]:
+    arr = x.copy()
+    history = []
+
+    for _ in range(step):
+        arr -= rate*gradient(f, arr)
+        history.append(arr.copy())
+
+    return arr, np.array(history)
 
 if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+
     print(gradient(func, np.array([3.0, 4.0])))
+    minimum, history = gradient_descent(func, np.array([-3.0, 4.0]), rate=0.1)
+    print(f"Minimum : {minimum}")
+    plt.plot([-5, 5], [0,0], '--b')
+    plt.plot([0,0], [-5, 5], '--b')
+    plt.plot(history[:,0], history[:,1], 'o')
+
+    plt.xlim(-3.5, 3.5)
+    plt.ylim(-4.5, 4.5)
+    plt.xlabel("X0")
+    plt.ylabel("X1")
+    plt.show()
