32c335e511a4d02b86a2f72531b961991f179207
Yumax-panda <yuma0wall@gmail.com>
2023-06-23 14:11:29 +0900

    fixed addlayer and implemented test code

--- ch05/layer_naive.py
+++ ch05/layer_naive.py
@@ -22,18 +22,26 @@ class MulLayer:
 
 class AddLayer:
 
-    if TYPE_CHECKING:
-        x: Optional[float]
-        y: Optional[float]
-
     def __init__(self) -> None:
-        self.x = None
-        self.y = None
+        pass
 
     def forward(self, x: float, y: float) -> float:
-        self.x = x
-        self.y = y
         return x + y
 
     def backward(self, dout: float) -> tuple[float, float]:
         return dout, dout
+
+
+if __name__ == "__main__":
+    apple = 100.0
+    apple_num = 2.0
+    tax = 1.1
+
+    mul = MulLayer()
+    mul_tax = MulLayer()
+    add = AddLayer()
+    row_price = mul.forward(apple, apple_num)
+    price = mul_tax.forward(row_price, tax)
+    dp, dtax = mul_tax.backward(1)
+    dapple, dnum = mul.backward(dp)
+    print(dp, dtax, dapple, dnum)
