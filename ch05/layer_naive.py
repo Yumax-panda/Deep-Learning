98d22d02d9b5209d283a812206b95a70b1b6854f
Yumax-panda <yuma0wall@gmail.com>
2023-06-23 14:26:37 +0900

    completed test code

--- ch05/layer_naive.py
+++ ch05/layer_naive.py
@@ -34,14 +34,27 @@ class AddLayer:
 
 if __name__ == "__main__":
     apple = 100.0
+    orange = 150.0
     apple_num = 2.0
+    orange_num = 3
     tax = 1.1
 
-    mul = MulLayer()
-    mul_tax = MulLayer()
-    add = AddLayer()
-    row_price = mul.forward(apple, apple_num)
-    price = mul_tax.forward(row_price, tax)
-    dp, dtax = mul_tax.backward(1)
-    dapple, dnum = mul.backward(dp)
-    print(dp, dtax, dapple, dnum)
+    apple_layer = MulLayer()
+    orange_layer = MulLayer()
+    total = AddLayer()
+    tax_layer = MulLayer()
+
+    # forward
+    apple_price = apple_layer.forward(apple, apple_num)
+    orange_price = orange_layer.forward(orange, orange_num)
+    total_price = total.forward(apple_price, orange_price)
+    tax_price = tax_layer.forward(tax, total_price)
+
+    # backward
+    dtax, dtotal_price = tax_layer.backward(1)
+    dapple_price, dorange_price = total.backward(dtotal_price)
+    dapple, dapple_num = apple_layer.backward(dapple_price)
+    dorange, dorange_num = orange_layer.backward(dorange_price)
+
+    print(apple_price, orange_price, total_price, tax_price)
+    print(dapple_price, dorange_price, dtotal_price, dtax, dapple_num, dorange_num, dapple, dorange)
